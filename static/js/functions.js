5dc434159ac2544b9c438f28641024cfa94b6b2a
Dee Koder <deekoder@users.noreply.github.com>
2017-04-28 12:15:31 -0700

    prtable: Add second table for open PRs. Fixed Apache License header. (#3)

--- static/js/functions.js
+++ static/js/functions.js
@@ -16,4 +16,19 @@ $(document).ready(function () {
             { data : "repository_url"}
         ]
     });
+
+    $('.table--data-1').DataTable({
+        "ajax": {
+            "url": "http://localhost:7000/getPRs",
+            "dataSrc": ""
+        },
+        "sDom": '<"dataTables__top"lf>rt<"dataTables__bottom"p><"clear">',
+        "columns": [
+            { data : "number" },
+            { data : "title" },
+            { data : "created_at" },
+            { data : "updated_at"},
+            { data : "repo_name"}
+        ]
+    });
 });
