28a2f5185e24d9a90511ab15730be4fc3f7318c4
Rushan <rushenn123@gmail.com>
2017-05-04 00:54:51 +0530

    Make rows clickable in 'issues' table (#16)
    * Add unversioned files to gitignore
    
    * Code clean and optimization
    
    * Make rows clickable in 'issues' table

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,21 +1,14 @@
 $(document).ready(function () {
 
-    // Data Table
-    $('#table-issues').DataTable({
+    // Issues Table
+    var dataTableIssues = $('#table-issues').DataTable({
         "ajax": {
             "url": "http://localhost:7000/getIssues",
             "dataSrc": ""
         },
-        "initComplete": function(settings, json) {
-            // ColResize
-            $(this).colResizable({
-                fixed:true,
-                liveDrag:true,
-                marginLeft: '5px'
-            });
-        },
         lengthMenu: [[15, 30, 60, 100, -1], ['15 Rows', '30 Rows', '60 Rows', '100 Rows', 'All']],
-        "sDom": '<"dataTables__top"lf>rt<"dataTables__bottom"p><"clear">',
+        "sDom": '<"dataTables__top"lf><"dataTables__inner"rt><"dataTables__bottom"p><"clear">',
+        "autoWidth": false,
         "columns": [
             { data : "number" },
             { data : "title" },
@@ -23,23 +16,25 @@ $(document).ready(function () {
             { data : "login" },
             { data : "milestone"},
             { data : "state" },
-            { data : "repository_url"}
+            { data : "repository_url"},
+            { data : "html_url"}
+        ],
+        "columnDefs": [
+            {
+                "targets": [ -1 ],
+                "visible": false,
+                "searchable": false
+            }
         ]
     });
 
+    // PR Table
     $('#table-prs').DataTable({
         "ajax": {
             "url": "http://localhost:7000/getPRs",
             "dataSrc": ""
         },
-        "initComplete": function(settings, json) {
-            // ColResize
-            $(this).colResizable({
-                fixed:true,
-                liveDrag:true,
-                marginLeft: '5px'
-            });
-        },
+        "autoWidth": false,
         lengthMenu: [[15, 30, 60, 100, -1], ['15 Rows', '30 Rows', '60 Rows', '100 Rows', 'All']],
         "sDom": '<"dataTables__top"lf>rt<"dataTables__bottom"p><"clear">',
         "columns": [
@@ -49,5 +44,19 @@ $(document).ready(function () {
             { data : "updated_at"},
             { data : "repo_name"}
         ]
+    });
+
+
+    // Table Column Resize
+    $('.table').colResizable({
+        liveDrag:true,
+        resizeMode: 'flex'
+    });
+
+
+    // Issues table row link
+    $('body').on('click', '#table-issues tbody tr', function () {
+        var data = dataTableIssues.row( this ).data();
+        window.open(data["html_url"], '_blank');
     });
 });
